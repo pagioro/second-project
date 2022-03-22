fe840a919b7cc2521ae89ce474ee91ecdb694317
pagioro <pagioro@hotmail.com>
2022-03-22 11:42:57 +0000

    Add js game into the index.js

---
+++ js/index.js
@@ -0,0 +1,61 @@
+const getUserChoice = function userInput() {
+  userInput = userInput.tolowercase();
+    if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
+      return userInput;
+    } else {
+      console.log('error!');
+      }
+    }
+
+function getComputerChoice() {
+  const randomNumber = Math.floor(Math.ramdom() * 3);
+    switch(randomNumber) {
+    case 0:
+    return 'rock';
+    break;
+    case 1:
+    return 'paper';
+    case 2:
+    return 'scissors';
+    break;
+    }
+  }
+
+function winner (userChoice, computerChoice) {
+   if (userChoice === computerChoice) {
+     return 'the game is a tie!';
+   }  
+   if (userChoice === 'rock') {
+     if (computerChoice === 'paper') {
+       return 'the computer won!';
+     } else {
+       return 'you won!';
+     }
+   }
+
+if (userChoice === 'paper') {
+  if (computerChoice === 'scissors') {
+    return 'the computer won!';
+  } else {
+    return 'you won!';
+  }
+}
+
+if (userChoice === 'scissors') {
+  if (computerChoice === 'rock') {
+    return 'the computer won!';
+  } else {
+    return 'you won!';
+    }
+  }
+}
+
+function playGame() {
+    const userChoice = getUserChoice('rock');
+    const computerChoice = getComputerChoice();
+    console.log("User:  " + userChoice);
+    console.log("Computer:  " + computerChoice);
+    console.log(determineWinner(userChoice, computerChoice));
+}
+
+playGame();
