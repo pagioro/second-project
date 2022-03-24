03e98ad0fa74a1057c559b788bb6ff12d0406448
pagioro <pagioro@hotmail.com>
2022-03-24 13:03:08 +0000

    I did everything again

--- js/index.js
+++ js/index.js
@@ -1,43 +1,63 @@
-
 let choice = document.getElementsByClassName('button');
 for (let i = 0; i < choice.length; i++) {
-    choice[i].addEventListener('click', game);
+  choice[i].addEventListener('click', game);
 }
 
-function game(userInput) { 
-    console.log(user.target.innerHTML) 
-    if (game === 'rock' || game === 'paper' || game === 'scissors') {
-      return game;
-    }
-  }
+function game(userInput) {  
+  let userChoice = userInput.target.innerHTML 
+  console.log(userChoice)
   
-function getComputerChoice() {  
-  let randomNumber = Math.floor(Math.random() * 3);
-    switch(randomNumber) {
-    case 0:
-    return 'rock';
-    case 1:
-    return 'paper';
-    case 2:
-    return 'scissors';
+  let computerChoice = getComputerChoice()
+  console.log(computerChoice)
+
+  if (userChoice === computerChoice) {
+    'tie'
     }
+    if (userChoice === 'paper') {
+      if (computerChoice === 'rock') {
+        'user won!'
+      } else if (computerChoice === 'scisor') {
+        'computer won!'
+      }
+        } else if (userChoice === 'rock') {
+            if (computerChoice === 'paper') {
+                'user won!'
+            } else if (computerChoice === 'scissors') {
+                'computer won!'
+            }
+          }
+      }
+
+/* return computer choice */  
+
+function getComputerChoice() {
+  let randomNumber = Math.floor(Math.random() * 3);
+  switch (randomNumber) {
+    case 0:
+      return 'rock';
+    case 1:
+      return 'paper';
+    case 2:
+      return 'scissors';
   }
+}
 
-  console.log(getComputerChoice());
 
 
-  let userGame = game();
+/*
+  let userGame = game(userInput);
   let computerGame = getComputerChoice();
 
-  if ((userGame === 'scissors' && computerGame === 'paper') || (userGame === 'scissors' && computerGame === 'paper') || (userGame === 'paper' && computerGame === 'rock')) {
+  if ((userGame === 'scissors' && computerGame === 'paper') || (userGame === 'rock' && computerGame === 'scissors') || (userGame === 'paper' && computerGame === 'rock')) {
     'you won!'
   } if (userGame === computerGame) {
     'the game is a tie!'
   } else {
     'the computer won!'
   }
+  */
 
-  /*
+/*
   
 function winner (userChoice, computerChoice) {
    if (userChoice === computerChoice) {
